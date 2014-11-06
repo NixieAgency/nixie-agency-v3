75d898d69eb18236780d89cec4a34ca33813e0c0
Melvin Gruesbeck <mgruesbeck@gmail.com>
2014-11-06 13:05:06 -0800

    use hogan partials

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+    { 
+      navType: 'hero',
+      partials: {footer: 'footer', header: 'header'}
+    });
 });
 
 router.get('/art', function(req, res) {
-  res.render('art', { title: 'Express' });
+  res.render('art', 
+    { 
+      navType: 'normal',
+      partials: {footer: 'footer', header: 'header'}
+    });
 });
 
 router.get('/technology', function(req, res) {
-  res.render('technology', { title: 'Express' });
+  res.render('technology', 
+    { 
+      navType: 'normal',
+      partials: {footer: 'footer', header: 'header'}
+    });
 });
 
 router.get('/contact', function(req, res) {
-  res.render('contact', { title: 'Express' });
+  res.render('contact', 
+    { 
+      navType: 'normal',
+      partials: {footer: 'footer', header: 'header'}
+    });
 });
 
 module.exports = router;
