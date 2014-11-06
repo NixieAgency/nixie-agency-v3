d43611ecfb0e60943e5763e0f739ca30880b81df
Melvin Gruesbeck <mgruesbeck@gmail.com>
2014-11-06 10:58:44 -0800

    add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/art', function(req, res) {
+  res.render('art', { title: 'Express' });
+});
+
+router.get('/technology', function(req, res) {
+  res.render('technology', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Express' });
 });
 
 module.exports = router;
